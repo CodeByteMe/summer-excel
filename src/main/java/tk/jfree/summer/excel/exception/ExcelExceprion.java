5930dc517146b823498829570178972aada46f56
ChengWei <dhweicheng@163.com>
2019-05-04 14:56:37 +0800

    更新 ExcelExceprion.java

--- src/main/java/tk/jfree/summer/excel/exception/ExcelExceprion.java
+++ src/main/java/tk/jfree/summer/excel/exception/ExcelExceprion.java
@@ -3,7 +3,7 @@ package tk.jfree.summer.excel.exception;
 /**
  * @author Cheng.Wei
  */
-public class ExcelExceprion extends RuntimeException{
+public class ExcelException extends RuntimeException{
     public ExcelExceprion() {
         super();
     }
